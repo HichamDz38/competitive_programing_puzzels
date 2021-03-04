4e15d49d99a0f20e7fa68230bfc71c0e985caa54
hichamdz38 <dachirhicham@gmail.com>
2021-03-04 03:30:49 +0100

    final

--- eight_qeens.py
+++ eight_qeens.py
@@ -1,12 +1,6 @@
 # solve right qeens problem using back-track technic
-<<<<<<< HEAD
+
 # we need a 8x8 matrix
 def solve():
     pass
-=======
 
-def solf
-
-
-ffff
->>>>>>> ecbb0231f214a26b7e3c9c10b53b8d4aa5a75ece
